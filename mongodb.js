1719e5ce79e33d447addd91fcf39290e1544458d
MAX <maxcleetus@gmail.com>
2025-03-01 12:52:49 +0530

    resposive almost fixed

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-mongoose.connect(process.env.MODULE)
+mongoose.connect("mongodb+srv://maxcleetus:mXAWtAcJbpt3L8Jc@datal.hb2ot.mongodb.net/?retryWrites=true&w=majority&appName=datal")
 .then(()=>{
     console.log("mongodb-connected")
 })
